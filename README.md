abbf228c6f80cb1bb9b884e24df9f6a390e85022
nilsme <tor3931@pm.me>
2021-09-14 01:17:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# README
+
+Using `code-server` with docker build from Fedora for local use.
+
+## Build images
+
+```Shell script
+docker-compose build
+```
+
+## Start code-server
+
+```Shell script
+docker-compose up -d
+```
+
+## Stop code-server
+
+```Shell script
+docker-compose down
+```
---
+++ code-server/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -eu
+
+if [ "${DOCKER_USER-}" ]; then
+  USER="$DOCKER_USER"
+  if [ "$DOCKER_USER" != "$(whoami)" ]; then
+    echo "$DOCKER_USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/nopasswd > /dev/null
+    # Unfortunately we cannot change $HOME as we cannot move any bind mounts
+    # nor can we bind mount $HOME into a new home as that requires a privileged container.
+    sudo usermod --login "$DOCKER_USER" coder
+    sudo groupmod -n "$DOCKER_USER" coder
+
+    sudo sed -i "/coder/d" /etc/sudoers.d/nopasswd
+  fi
+fi
+
+/usr/bin/code-server --disable-telemetry --bind-addr 0.0.0.0:8080 --auth none
